d18421d9a37f5f02f430b61a16734cc2a5f832dc
Alfredo Alas <alfredo.alas14@gmail.com>
2024-11-26 18:38:30 -0600

    add project files

---
+++ src/test/java/biz/utilsdev/qrgenerator/QrGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.utilsdev.qrgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
